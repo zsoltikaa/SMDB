b658b23a72563ed8a9ab2d6f9f48fb7c45aacf1a
zsoltikaa <padarzsolti@gmail.com>
2025-04-10 20:44:11 +0200

    added imdb_rating sorting option

--- assets/js/MainController.js
+++ assets/js/MainController.js
@@ -2,8 +2,6 @@ app.controller('MainController', function($scope, $http) {
 
     $http.get("http://localhost/SMDB/assets/php/smdb.php")
         .then(function(response) {
-            console.log(response.data);
-
             $scope.entries = response.data;
         })
         .catch(function(error) {
