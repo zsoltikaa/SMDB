806a169bd7101d7e8853a0f5074bf0a2f46015fe
zsoltikaa <padarzsolti@gmail.com>
2025-04-15 20:29:49 +0200

    redesigned the folder structure

---
+++ frontend/js/MainController.js
@@ -0,0 +1,24 @@
+app.controller('MainController', function($scope, $http) {
+
+    $http.get("http://localhost/SMDB/backend/api/smdb.php")
+        .then(function(response) {
+            $scope.entries = response.data;
+        })
+        .catch(function(error) {
+            console.error('Error when loading data:', error);
+        });
+
+        $scope.sortBy = "";
+
+        $scope.startsWithFilter = function(item) {
+            if (!$scope.searchTitle || $scope.searchTitle.trim() === '') {
+                return true;
+            }
+        
+            const title = (item.title || '').toLowerCase();
+            const search = $scope.searchTitle.toLowerCase();
+        
+            return title.indexOf(search) === 0;
+        };
+
+});
---
+++ frontend/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    tailwind.config = {
+        darkMode: 'class',
+    }
+});
